4668171f3509b627e7504e9691e72d432bc0d18b
Gautam <pbg2402@gmail.com>
2021-11-14 22:30:26 +0530

    Initial commit

--- components/MiddleBar.js
+++ components/MiddleBar.js
@@ -6,20 +6,39 @@ import { auth, db } from '../firebase'
 import MiddleComponent from './MiddleComponent'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import * as EmailValidator from 'email-validator'
+import { useCollection } from 'react-firebase-hooks/firestore'
 
 const MiddleBar = () => {
     const [user] = useAuthState(auth)
 
-    const [chat, setChat] = useState('')
+    const chatRef = db
+        .collection('chats')
+        .where('users', 'array-contains', user.email)
+
+    const [chatsSnapshot] = useCollection(chatRef)
+
+    const [email, setEmail] = useState('')
+
+    const checkChatExists = (recipientEmail) => {
+        return chatsSnapshot?.docs.find(
+            (chat) =>
+                chat.data().users.find((user) => user === recipientEmail)
+                    ?.length > 0
+        )
+    }
 
     const createChat = (e) => {
         e.preventDefault()
-        if (EmailValidator.validate(chat) && chat !== user.email) {
+        if (
+            EmailValidator.validate(email) &&
+            email !== user.email &&
+            !checkChatExists(email)
+        ) {
             db.collection('chats').add({
-                users: [user.email, chat],
+                users: [user.email, email],
             })
         }
-        setChat('')
+        setEmail('')
     }
 
     return (
@@ -27,8 +46,8 @@ const MiddleBar = () => {
             <div className="space-y-3">
                 <form>
                     <input
-                        value={chat}
-                        onChange={(e) => setChat(e.target.value)}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         placeholder="Start a conversation"
                         className="bg-[#202225] rounded-lg px-2 py-1 text-sm w-full text-white"
                     />
@@ -64,6 +83,9 @@ const MiddleBar = () => {
                     image="https://cdn.britannica.com/54/188754-050-A3613741/Elon-Musk-2010.jpg"
                     title="Pranav Satyababu"
                 />
+                {/* {chatsSnapshot.docs.map((chat) => (
+                    <MiddleComponent key = {chat.id} image = {chat.photoURL} />
+                ))} */}
             </div>
         </div>
     )
--- components/MiddleComponent.js
+++ components/MiddleComponent.js
@@ -1,11 +1,14 @@
 import { Avatar } from '@material-ui/core'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import getChats from '../utils/getChats'
 
-const MiddleComponent = ({ image, title }) => {
+const MiddleComponent = ({ id, users }) => {
     return (
         <div className="flex items-center space-x-2 group">
-            <Avatar src={image} alt={title} />
+            {/* <Avatar src={image} alt={title} /> */}
+            <Avatar />
             <p className="text-gray-400 group-hover:text-white cursor-pointer">
-                {title}
+                User 1
             </p>
         </div>
     )

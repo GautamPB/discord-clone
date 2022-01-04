8b13ca705c93ee017d503de36cf56cdeb9882f0a
Gautam <pbg2402@gmail.com>
2022-01-05 02:00:14 +0530

    Added: server options dropdown

--- components/MiddleBar.js
+++ components/MiddleBar.js
@@ -1,7 +1,7 @@
 //render DMs first.
 //render channels once server has been selected.
 import { useState } from 'react'
-import { Avatar } from '@material-ui/core'
+// import { Avatar } from '@material-ui/core'
 import { auth, db } from '../firebase'
 import MiddleComponent from './MiddleComponent'
 import { useAuthState } from 'react-firebase-hooks/auth'
@@ -9,6 +9,10 @@ import * as EmailValidator from 'email-validator'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { useSelector } from 'react-redux'
 import { selectActiveServer } from '../slices/activeServerSlice'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import PersonAddIcon from '@material-ui/icons/PersonAdd'
+import { PlusIcon } from '@heroicons/react/solid'
+import { LogoutIcon } from '@heroicons/react/solid'
 
 const MiddleBar = ({ middleBarData, dataType }) => {
     const [user] = useAuthState(auth)
@@ -24,6 +28,8 @@ const MiddleBar = ({ middleBarData, dataType }) => {
     const [email, setEmail] = useState('')
 
     const [activeChannel, setActiveChannel] = useState('general')
+
+    const [openDropdown, setOpenDropdown] = useState(false)
 
     const checkChatExists = (recipientEmail) => {
         return chatsSnapshot?.docs.find(
@@ -51,10 +57,36 @@ const MiddleBar = ({ middleBarData, dataType }) => {
         <div className="bg-[#33363C] w-full pb-4 items-center space-y-3 h-full">
             {middleBarData && dataType === 'server' ? (
                 <>
-                    <div className="py-3 bg-[#33363C] shadow-lg border-b-2 border-[#36393F]">
-                        <p className="text-white font-semibold px-3">
+                    <div
+                        className="py-3 bg-[#33363C] shadow-lg border-b-2 border-[#36393F] relative flex items-center cursor-pointer"
+                        onClick={() => setOpenDropdown(!openDropdown)}
+                    >
+                        <p className="text-white font-semibold px-3 flex-1">
                             {activeServer.serverName}
                         </p>
+
+                        <ChevronDownIcon className="h-5 text-white mr-4" />
+
+                        {!openDropdown ? (
+                            <div className="flex flex-col absolute bg-[#18191C] p-4 rounded-md top-[4rem] w-[90%] left-3 space-y-3">
+                                <div className="serverOptions text-[#3F51B5] ">
+                                    <p className="flex-1">Invite People</p>
+                                    <PersonAddIcon className="text-[10px] " />
+                                </div>
+
+                                <div className="serverOptions text-white">
+                                    <p className="flex-1">Create Channel</p>
+                                    <PlusIcon className="h-6" />
+                                </div>
+
+                                <div className="serverOptions text-[#B71C1C]">
+                                    <p className="flex-1">Leave Server</p>
+                                    <LogoutIcon className="h-6" />
+                                </div>
+                            </div>
+                        ) : (
+                            ''
+                        )}
                     </div>
 
                     <div className="space-y-3 px-4">
@@ -64,9 +96,14 @@ const MiddleBar = ({ middleBarData, dataType }) => {
                     </div>
 
                     <div className="px-2">
-                        <div>
+                        <div className="flex flex-col space-y-3">
                             {middleBarData.map((channelData) => (
                                 <p
+                                    onClick={() =>
+                                        setActiveChannel(
+                                            channelData.channelName
+                                        )
+                                    }
                                     className={`${
                                         channelData.channelName ===
                                         activeChannel
